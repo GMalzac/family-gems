27b87ce575a6962137547f642d48626369002e65
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-05-19 17:11:04 +0100

    Update schema with members

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_154824) do
+ActiveRecord::Schema.define(version: 2021_05_19_160048) do
 
   create_table "conversations", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2021_05_19_154824) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "member_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_messages_on_member_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_05_19_154824) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "messages", "members"
 end
