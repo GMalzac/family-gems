3641672e004113451029ff422dd2dab887309859
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-05-19 17:36:35 +0100

    Add show view for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  def index
+    @admin_groups = Groupe.where(user_id: current_user.id)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+    @group.save
+    redirect_to group_path(@group)
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
