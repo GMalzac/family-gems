a4cce867741a964270ec944ba9d64ec92cb099df
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-05-19 17:15:29 +0100

    Generate groups controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,6 +2,7 @@ class ConversationsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @admin_groups = Group.where(user_id: current_user.id)
     @conversations = Conversation.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'conversations#index'
-  get 'conversations', to: 'conversations#index'
+  resources :groups do
+    resources :conversations
+  end
 end
